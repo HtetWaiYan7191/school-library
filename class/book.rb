183132417b82bc5c1ec77fd63cdb4bf1f7d354af
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-28 20:41:41 +0630

    Add add_rental method

--- class/book.rb
+++ class/book.rb
@@ -1,3 +1,4 @@
+require_relative 'rental'
 class Book
   attr_accessor :title, :author, :rental
 
@@ -5,5 +6,9 @@ class Book
     @title = title
     @author = author
     @rental = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
   end
 end
